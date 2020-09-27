faaa48aff15c1a49ba6319d1d71dfaa2617bcabc
hongyouheng <26552699@qq.com>
2020-09-27 14:52:38 +0800

    add 处理图片

---
+++ 1.webpack核心功能/1-13.练习处理图片/src/index.js
@@ -0,0 +1,7 @@
+var src = require("./assets/webpack.png")
+
+var img = document.createElement("img")
+img.src = src
+document.body.appendChild(img)
+
+
---
+++ 1.webpack核心功能/1-13.练习处理图片/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: "development",
+    module: {
+        rules: [{
+            test: /\.(png)|(jpg)|(gif)$/,
+            use: [{
+                loader: './loaders/img-loader.js',
+                options: {
+                    limit: 3000, //3000字节以上使用图片，3000字节以内使用base64
+                    filename: "img-[contenthash:5].[ext]"
+                }
+            }]
+        }]
+    }
+
+}
