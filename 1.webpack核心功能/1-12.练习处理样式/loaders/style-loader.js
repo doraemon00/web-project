bd52bbf84c7f10538bb49b94f9ad874caedb8879
hongyouheng <26552699@qq.com>
2020-09-27 14:31:21 +0800

    add loader处理样式

---
+++ 1.webpack核心功能/1-12.练习处理样式/loaders/style-loader.js
@@ -0,0 +1,8 @@
+module.exports = function(sourceCode){
+    var code = `var style = document.createElement("style");
+    style.innerHTML = \`${sourceCode}\`;
+    document.head.appendChild(style);
+    module.exports = \`${sourceCode}\``;
+    return code;
+
+}
