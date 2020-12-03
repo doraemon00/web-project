c1b622cbb7a0ac8cc480182d525830a21053be4c
hongyouheng <26552699@qq.com>
2020-12-03 13:01:53 +0800

    add  在webpack 中使用babel

---
+++ 4.js兼容性/4-4.在webpack中使用babel/dist/main.js
@@ -0,0 +1,14 @@
+/******/ (function() { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+var func = function func() {
+  console.log("abc");
+};
+
+func();
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
